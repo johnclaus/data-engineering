acd8b09997333e9a236e0563c69f494a27f17098
John Claus <johnclaus@gmail.com>
2013-06-13 21:16:33 -0600

    Rails 4 scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataEngineering::Application.config.secret_key_base = '49531401070f301691ffbfa6997396cfcb45c9ec501acb571020e4e0bb1bc36ce4ce7a4a48939e17864e12212ad020dd00559a3898e9911d550838861c276db6'
