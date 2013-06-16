29fbab5d728288c361630607fb24b41ab2582428
John Claus <johnclaus@gmail.com>
2013-06-16 15:48:27 -0600

    Added Item model, but bound to change

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,15 @@
 class Item < ActiveRecord::Base
   belongs_to :merchant
   validates :merchant_id, presence: true
-  
-  validates :price, presence: true
-  validates_numericality_of :price, :greater_than => 0
+
+  composed_of :price,
+              :class_name => "Money",
+              :mapping => [%w(price_cents cents), %w(currency_type currency_as_string)],
+              :constructor => Proc.new { |cents, currency_type | 
+                Money.new(cents || 0, currency_type|| Money.default_currency) },
+              :converter => Proc.new { |value| 
+                value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, 
+                "Unable to convert #{value.class} into Money") }
   
   validates :description, uniqueness: true, presence: true
 end
