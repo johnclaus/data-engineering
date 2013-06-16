a1557d4445e1c62b9480e208a557eff9769e751e
John Claus <johnclaus@gmail.com>
2013-06-16 15:15:31 -0600

    Further schema changes

--- db/migrate/20130614235414_create_items.rb
+++
@@ -1,8 +0,0 @@
-class CreateItems < ActiveRecord::Migration
-  def change
-    create_table :items do |t|
-      t.string :description
-      t.decimal :price
-    end
-  end
-end
--- db/migrate/20130615064852_add_merchant_id_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddMerchantIdToItem < ActiveRecord::Migration
-  def change
-    add_column :items, :merchant_id, :integer
-  end
-end
