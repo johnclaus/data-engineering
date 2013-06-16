19b9e0c3ddc0b0b57f2e644169a64e6e178e3f0f
John Claus <johnclaus@gmail.com>
2013-06-16 16:11:06 -0600

    Added my uploader library; still debugging an error caused during job queueing

---
+++ lib/uploader.rb
@@ -0,0 +1,41 @@
+require 'csv'
+class Uploader
+  @queue = "uploader"
+
+  def self.logger
+    Rails.logger
+  end
+  
+  def self.perform(subsidiary_data_id)
+    subsidiary_data = SubsidiaryData.find(subsidiary_data_id).limit(1)
+    subsidiary_data.begin_processing_time
+    process_csv(subsidiary_data.uploaded_data.current_path)
+    subsidiary_data.complete_processing
+  end
+  
+  def self.process_csv(upload_file_path)
+    CSV.foreach(upload_file_path, headers: true, col_sep: "\t", ) do |row|
+      merchant_data = {name: row["merchant name"], address: row["merchant address"]}
+      item_data = {description: row["item description"], price: row["item price"]}
+      purchaser_data = {name: row["purchaser name"]}
+      purchase_count = row["purchase count"]
+
+      begin
+        merchant = Merchant.where(merchant_data).first
+        merchant = Merchant.create!(merchant_data) unless merchant
+        
+        item = Item.where(merchant_id: merchant, description: item_data[:description]).first
+        item = Item.create!(item_data.merge(merchant: merchant)) unless item
+
+        purchaser = Purchaser.where(purchaser_data).first
+        purchaser = Purchaser.create!(purchaser_data) unless purchaser
+
+        purchase_count.to_i.times do
+          Purchase.create!(purchaser: purchaser, item: item)
+        end
+      rescue ActiveRecord::RecordInvalid => e
+        logger.error e.inspect
+      end
+    end
+  end
+end
