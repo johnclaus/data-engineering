7df393c9674928f5638c64c77cb815c3beff78c2
John Claus <johnclaus@gmail.com>
2013-06-16 15:21:40 -0600

    Routing change

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ require 'resque/server'
 DataEngineering::Application.routes.draw do
   resources :subsidiary_data
   mount Resque::Server.new, :at => "/resque"
-  root :to => 'subsidiary_data#new'
+  root :to => 'subsidiary_data#index'
 end
