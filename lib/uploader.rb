cfe8aeb3a44e5003941fba9b02be57a3d8cba706
John Claus <johnclaus@gmail.com>
2014-04-14 21:09:47 -0600

    Figuring out how Code Climate code smells and complexity work

--- lib/uploader.rb
+++ lib/uploader.rb
@@ -15,23 +15,21 @@ class Uploader
   
   def self.process_csv(upload_file_path)
     CSV.foreach(upload_file_path, headers: true, col_sep: "\t", ) do |row|
-      merchant_data = {name: row["merchant name"], address: row["merchant address"]}
-      item_data = {description: row["item description"], price: row["item price"]}
-      purchaser_data = {name: row["purchaser name"]}
+      merchant_data = {:name => row["merchant name"], :address => row["merchant address"]}
+      item_data = {:description => row["item description"], :price => row["item price"]}
+      purchaser_data = {:name => row["purchaser name"]}
       purchase_count = row["purchase count"]
 
       begin
-        merchant = Merchant.where(merchant_data).first
-        merchant = Merchant.create!(merchant_data) unless merchant
-        
-        item = Item.where(merchant_id: merchant, description: item_data[:description]).first
-        item = Item.create!(item_data.merge(merchant: merchant)) unless item
+        merchant = Merchant.where(merchant_data).first_or_create
 
-        purchaser = Purchaser.where(purchaser_data).first
-        purchaser = Purchaser.create!(purchaser_data) unless purchaser
+        item = Item.where(:merchant_id => merchant, :description => item_data[:description]).first
+        item = Item.create!(item_data.merge(:merchant => merchant)) unless item
+
+        purchaser = Purchaser.where(purchaser_data).first_or_create
 
         purchase_count.to_i.times do
-          Purchase.create!(purchaser: purchaser, item: item)
+          Purchase.create!(:purchaser => purchaser, :item => item)
         end
       rescue ActiveRecord::RecordInvalid => e
         logger.error e.inspect
